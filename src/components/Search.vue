683c667e0cf3c9ea6a67071a2765cc25e3880c4d
Bekzat <bekzat.bagdatuly03@gmail.com>
2022-06-15 05:35:47 +0600

    vue

---
+++ src/components/Search.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="flex w-full h-full">
+		<SearchInput />
+		<SearchButton />
+	</div>
+</template>
+
+<script>
+import SearchInput from './SearchInput.vue'
+import SearchButton from './SearchButton.vue'
+
+export default {
+	components: {
+		SearchInput,
+		SearchButton
+	}
+}
+</script>
---
+++ src/components/SearchInput.vue
@@ -0,0 +1,3 @@
+<template>
+	<input class="border border-gray-300 w-full px-3 shadow-inner rounded-bl-sm rounded-tl-sm focus:border-blue-700 focus:outline-none" placeholder="Search" type="text">
+</template>
