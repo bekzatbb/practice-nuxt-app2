84ed738dc1fcc6db18b41df5b41e128a7f7a28c0
Bekzat <bekzat.bagdatuly03@gmail.com>
2022-06-27 21:35:04 +0600

    vue

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,51 +2,16 @@
 	<aside class="hidden xl:block w-64 max-h-screen overflow-auto fixed top-0 pt-12 bg-white z-20">
     <section class="py-2 border-b">
       <ul>
-        <li>
-          <a href="#" class="flex items-center px-6 py-2 text-sm font-medium text-gray-800 bg-gray-200 hover:bg-gray-300">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-6 text-red-600" viewBox="0 0 20 20" fill="currentColor">
-              <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-            </svg>
-            <span>Home</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="flex items-center px-6 py-2 text-sm hover:bg-gray-100">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-6" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd" d="M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z" clip-rule="evenodd" />
-            </svg>
-            <span>Trending</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="flex items-center px-6 py-2 text-sm hover:bg-gray-100">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-6" viewBox="0 0 20 20" fill="currentColor">
-              <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" />
-            </svg>
-            <span>Subscriptions</span>
-          </a>
-        </li>
+        <SidebarNavItem label="Home" icon="home" is-active />
+        <SidebarNavItem label="Trending" icon="fire" />
+        <SidebarNavItem label="Shorts" icon="fire" />
+        <SidebarNavItem label="Subscriptions" icon="collection" />
       </ul>
     </section>
     <section class="py-2 border-b">
       <ul>
-        <li>
-          <a href="#" class="flex items-center px-6 py-2 text-sm hover:bg-gray-100">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-6" viewBox="0 0 20 20" fill="currentColor">
-              <path d="M9 2a2 2 0 00-2 2v8a2 2 0 002 2h6a2 2 0 002-2V6.414A2 2 0 0016.414 5L14 2.586A2 2 0 0012.586 2H9z" />
-              <path d="M3 8a2 2 0 012-2v10h8a2 2 0 01-2 2H5a2 2 0 01-2-2V8z" />
-            </svg>
-            <span>Library</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="flex items-center px-6 py-2 text-sm hover:bg-gray-100">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-6" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd" />
-            </svg>
-            <span>History</span>
-          </a>
-        </li>
+        <SidebarNavItem label="Library" icon="duplicate" />
+        <SidebarNavItem label="History" icon="clock" />
       </ul>
     </section>
     <section class="px-8 py-4 font-medium leading-5 border-b">
@@ -65,90 +30,18 @@
         BEST OF YOUTUBE
       </div>
       <ul>
-        <li>
-          <a href="#" class="flex items-center px-6 py-2 text-sm hover:bg-gray-100">
-            <div class="p-1 mr-6 bg-gray-700 rounded-full">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-white" viewBox="0 0 20 20" fill="currentColor">
-                <path d="M18 3a1 1 0 00-1.196-.98l-10 2A1 1 0 006 5v9.114A4.369 4.369 0 005 14c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V7.82l8-1.6v5.894A4.37 4.37 0 0015 12c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V3z" />
-              </svg>
-            </div>
-            <span>Music</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="flex items-center px-6 py-2 text-sm hover:bg-gray-100">
-            <div class="p-1 mr-6 bg-gray-700 rounded-full">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-white" viewBox="0 0 20 20" fill="currentColor">
-                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-2 0c0 .993-.241 1.929-.668 2.754l-1.524-1.525a3.997 3.997 0 00.078-2.183l1.562-1.562C15.802 8.249 16 9.1 16 10zm-5.165 3.913l1.58 1.58A5.98 5.98 0 0110 16a5.976 5.976 0 01-2.516-.552l1.562-1.562a4.006 4.006 0 001.789.027zm-4.677-2.796a4.002 4.002 0 01-.041-2.08l-.08.08-1.53-1.533A5.98 5.98 0 004 10c0 .954.223 1.856.619 2.657l1.54-1.54zm1.088-6.45A5.974 5.974 0 0110 4c.954 0 1.856.223 2.657.619l-1.54 1.54a4.002 4.002 0 00-2.346.033L7.246 4.668zM12 10a2 2 0 11-4 0 2 2 0 014 0z" clip-rule="evenodd" />
-              </svg>
-            </div>
-            <span>Sports</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="flex items-center px-6 py-2 text-sm hover:bg-gray-100">
-            <div class="p-1 mr-6 bg-gray-700 rounded-full">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-white" viewBox="0 0 20 20" fill="currentColor">
-                <path d="M10 3.5a1.5 1.5 0 013 0V4a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-.5a1.5 1.5 0 000 3h.5a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-.5a1.5 1.5 0 00-3 0v.5a1 1 0 01-1 1H6a1 1 0 01-1-1v-3a1 1 0 00-1-1h-.5a1.5 1.5 0 010-3H4a1 1 0 001-1V6a1 1 0 011-1h3a1 1 0 001-1v-.5z" />
-              </svg>
-            </div>
-            <span>Gaming</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="flex items-center px-6 py-2 text-sm hover:bg-gray-100">
-            <div class="p-1 mr-6 bg-gray-700 rounded-full">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-white" viewBox="0 0 20 20" fill="currentColor">
-                <path fill-rule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm3 2h6v4H7V5zm8 8v2h1v-2h-1zm-2-2H7v4h6v-4zm2 0h1V9h-1v2zm1-4V5h-1v2h1zM5 5v2H4V5h1zm0 4H4v2h1V9zm-1 4h1v2H4v-2z" clip-rule="evenodd" />
-              </svg>
-            </div>
-            <span>Movies</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="flex items-center px-6 py-2 text-sm hover:bg-gray-100">
-            <div class="p-1 mr-6 bg-gray-700 rounded-full">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-white" viewBox="0 0 20 20" fill="currentColor">
-                <path fill-rule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clip-rule="evenodd" />
-                <path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z" />
-              </svg>
-            </div>
-            <span>News</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="flex items-center px-6 py-2 text-sm hover:bg-gray-100">
-            <div class="p-1 mr-6 bg-gray-700 rounded-full">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M5.636 18.364a9 9 0 010-12.728m12.728 0a9 9 0 010 12.728m-9.9-2.829a5 5 0 010-7.07m7.072 0a5 5 0 010 7.07M13 12a1 1 0 11-2 0 1 1 0 012 0z" />
-              </svg>
-            </div>
-            <span>Live</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="flex items-center px-6 py-2 text-sm hover:bg-gray-100">
-            <div class="p-1 mr-6 bg-gray-700 rounded-full">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-white" viewBox="0 0 20 20" fill="currentColor">
-                <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
-              </svg>
-            </div>
-            <span>360&deg; Video</span>
-          </a>
-        </li>
+        <SidebarNavItem label="Music" icon="musicNote" />
+        <SidebarNavItem label="Sports" icon="speakerphone" />
+        <SidebarNavItem label="Gaming" icon="puzzle" />
+        <SidebarNavItem label="Movies" icon="film" />
+        <SidebarNavItem label="News" icon="newspaper" />
+        <SidebarNavItem label="Live" icon="statusOnline" />
+        <SidebarNavItem label="360&deg; Video" icon="eye" />
       </ul>
     </section>
     <section class="py-2 border-b">
       <ul>
-        <li>
-          <a href="#" class="flex items-center px-6 py-2 text-sm hover:bg-gray-100">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-6" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd" />
-            </svg>
-            <span>Browse channels</span>
-          </a>
-        </li>
+        <SidebarNavItem label="Browse Channels" icon="plusCircle" />
       </ul>
     </section>
     <section class="py-2 border-b">
@@ -156,60 +49,16 @@
         More From Youtube
       </div>
       <ul>
-        <li>
-          <a href="#" class="flex items-center px-6 py-2 text-sm hover:bg-gray-100">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-6" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
-            </svg>
-            <span>YouTube Premium</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="flex items-center px-6 py-2 text-sm hover:bg-gray-100">
-            <div class="p-1 mr-6 bg-gray-700 rounded-full">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M5.636 18.364a9 9 0 010-12.728m12.728 0a9 9 0 010 12.728m-9.9-2.829a5 5 0 010-7.07m7.072 0a5 5 0 010 7.07M13 12a1 1 0 11-2 0 1 1 0 012 0z" />
-              </svg>
-            </div>
-            <span>Live</span>
-          </a>
-        </li>
+        <SidebarNavItem label="YouTube Premium" icon="play" />
+        <SidebarNavItem label="Live" icon="statusOnline" />
       </ul>
     </section>
     <section class="py-2 border-b">
       <ul>
-        <li>
-          <a href="#" class="flex items-center px-6 py-2 text-sm hover:bg-gray-100">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-6" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
-            </svg>
-            <span>Settings</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="flex items-center px-6 py-2 text-sm hover:bg-gray-100">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-6" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd" d="M3 6a3 3 0 013-3h10a1 1 0 01.8 1.6L14.25 8l2.55 3.4A1 1 0 0116 13H6a1 1 0 00-1 1v3a1 1 0 11-2 0V6z" clip-rule="evenodd" />
-            </svg>
-            <span>Report history</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="flex items-center px-6 py-2 text-sm hover:bg-gray-100">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-6" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
-            </svg>
-            <span>Help</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="flex items-center px-6 py-2 text-sm hover:bg-gray-100">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-6" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clip-rule="evenodd" />
-            </svg>
-            <span>Send feedback</span>
-          </a>
-        </li>
+        <SidebarNavItem label="Settings" icon="cog" />
+        <SidebarNavItem label="Report history" icon="flag" />
+        <SidebarNavItem label="Help" icon="questionMarkCircle" />
+        <SidebarNavItem label="Send feedback" icon="annotation" />
       </ul>
     </section>
     <section class="px-6 py-4 text-xs font-semibold space-y-4">
@@ -235,3 +84,13 @@
     </section>
   </aside>
 </template>
+
+<script>
+import SidebarNavItem from './SidebarNavItem.vue'
+
+export default {
+  components: {
+    SidebarNavItem
+  }
+}
+</script>
