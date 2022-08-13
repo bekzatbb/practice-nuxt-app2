04c1c3ae9ecea0440c7506712be4a9a7c5c0faf7
Bekzat <bekzat.bagdatuly03@gmail.com>
2022-08-13 21:04:28 +0600

    react

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,17 +12,23 @@
     </div>
     <div class="hidden sm:flex items-center justify-end p-2.5 pl-8 md:pl-12 md:px-8 flex-1 lg:px-0 lg:w-1/2 max-w-screen-md">
       <Search />
-      <button class="ml-4 focus:outline-none">
-        <BaseIcon name="microphone" class="w-5 h-5" />
-      </button>
+      <BaseTooltip text="Search with your voice">
+        <button class="p-2 focus:outline-none">
+          <BaseIcon name="microphone" class="w-5 h-5" />
+        </button>
+      </BaseTooltip>
     </div>
     <div class="flex items-center justify-end lg:w-1/4 space-x-3 p-2 sm:px-4">
-      <button class="sm:hidden p-2 focus:outline-none">
-        <BaseIcon name="microphone" class="w-5 h-5" />
-      </button>
-      <button class="sm:hidden p-2 focus:outline-none">
-        <BaseIcon name="search" class="w-5 h-5" />
-      </button>
+      <BaseTooltip text="Search with your voice">
+        <button class="sm:hidden p-2 focus:outline-none">
+          <BaseIcon name="microphone" class="w-5 h-5" />
+        </button>
+      </BaseTooltip>
+      <BaseTooltip text="Search">
+        <button class="sm:hidden p-2 focus:outline-none">
+          <BaseIcon name="search" class="w-5 h-5" />
+        </button>
+      </BaseTooltip>
       <button class="relative group p-2 focus:outline-none">
         <BaseIcon name="viewGrid" class="w-5 h-5" />
         <DropdownApps />
@@ -41,6 +47,7 @@
 <script>
 import LogoMain from './LogoMain.vue'
 import BaseIcon from './BaseIcon.vue'
+import BaseTooltip from './BaseTooltip.vue'
 import LoginButton from './LoginButton.vue'
 import Search from './Search.vue'
 import DropdownApps from './DropdownApps.vue'
@@ -50,6 +57,7 @@ export default {
 	components: {
 		LogoMain,
     BaseIcon,
+    BaseTooltip,
 		LoginButton,
 		Search,
 		DropdownApps,
