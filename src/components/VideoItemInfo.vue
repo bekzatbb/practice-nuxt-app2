ccb122ca7d1410c2f6963d6e14eb48f544fb00b0
Bekzat <bekzat.bagdatuly03@gmail.com>
2022-08-14 15:57:23 +0600

    vue

--- src/components/VideoItemInfo.vue
+++ src/components/VideoItemInfo.vue
@@ -3,9 +3,13 @@
     <img :src="`https://picsum.photos/seed/avatar${index}/68`" class="mr-3 rounded-full w-9 h-9" alt="">
     <div class="text-sm">
       <span class="font-semibold text-gray-800">Video title {{ index }}</span>
-      <div class="mt-1 flex items-center">
-        <span>Channel name {{ index }}</span>
-        <BaseIcon name="checkCircle" class="w-3.5 h-3.5 ml-1" />
+      <div class="mt-1 flex">
+      	<BaseTooltip :text="channelName" top>
+      		<span>{{ channelName }}</span>
+      	</BaseTooltip>
+      	<BaseTooltip text="Verified" top>
+      		<BaseIcon name="checkCircle" class="w-3.5 h-3.5 ml-1" />
+      	</BaseTooltip>
       </div>
       <div v-html="summary"></div>
     </div>
@@ -16,13 +20,22 @@
 <script>
 import BaseIcon from './BaseIcon.vue'
 import VideoItemDropdown from './VideoItemDropdown.vue'
+import BaseTooltip from './BaseTooltip.vue'
 
 export default {
 	components: {
-		BaseIcon,
-		VideoItemDropdown
+    BaseIcon,
+    VideoItemDropdown,
+    BaseTooltip
 	},
 	props: ['index'],
+
+	data () {
+		return {
+			channelName: `Channel name ${this.index}`
+		}
+	},
+
 	computed: {
 		summary () {
 			const days = this.index === 1 ? 'day' : 'days'
