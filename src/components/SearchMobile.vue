923d4567b09f5967ea5f25e2926d72cec581df81
Bekzat <bekzat.bagdatuly03@gmail.com>
2022-08-14 23:27:23 +0600

    vue

--- src/components/SearchMobile.vue
+++ src/components/SearchMobile.vue
@@ -1,13 +1,29 @@
 <template>
-  
+  <div class="fixed bg-white w-full h-14 p-3 z-10 flex">
+  	<BaseTooltip text="Back" right>
+  		<button class="p-2 focus:outline-none">
+  			<BaseIcon name="arrowLeft" class="w-5 h-5" />
+  		</button>
+  	</BaseTooltip>
+  	<Search />
+  	<BaseTooltip text="Search with your voice" left>
+  		<button class="p-2 focus:outline-none">
+  			<BaseIcon name="microphone" class="w-5 h-5" />
+  		</button>
+  	</BaseTooltip>
+  </div>
 </template>
 
 <script>
-export default {
+import BaseTooltip from './BaseTooltip.vue';
+import BaseIcon from './BaseIcon.vue';
+import Search from './Search.vue';
 
+export default {
+    components: { 
+    	BaseTooltip, 
+    	BaseIcon, 
+    	Search 
+    }
 }
 </script>
-
-<style>
-
-</style>
