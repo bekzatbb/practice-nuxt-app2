ae289001c234313b79e2b2b9cdebeadfd6826430
Bekzat <bekzat.bagdatuly03@gmail.com>
2022-08-14 23:21:58 +0600

    vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,6 +10,7 @@
         <LogoMain />
       </div>
     </div>
+    <SearchMobile />
     <div class="hidden sm:flex items-center justify-end p-2.5 pl-8 md:pl-12 md:px-8 flex-1 lg:px-0 lg:w-1/2 max-w-screen-md">
       <Search />
       <BaseTooltip text="Search with your voice">
@@ -50,19 +51,22 @@ import BaseIcon from './BaseIcon.vue'
 import BaseTooltip from './BaseTooltip.vue'
 import LoginButton from './LoginButton.vue'
 import Search from './Search.vue'
+import SearchMobile from './SearchMobile.vue'
 import DropdownApps from './DropdownApps.vue'
 import DropdownSettings from './DropdownSettings.vue'
 
 export default {
 	components: {
-		LogoMain,
+    LogoMain,
     BaseIcon,
     BaseTooltip,
-		LoginButton,
-		Search,
-		DropdownApps,
-		DropdownSettings
-	},
+    LoginButton,
+    Search,
+    SearchMobile,
+    DropdownApps,
+    DropdownSettings,
+    Search
+},
   emits: {
     toggleSidebar: null
   }
