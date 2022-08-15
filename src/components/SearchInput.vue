c2d86c7249f90acb2f59ef7aa0a71df48a895e1c
Bekzat <bekzat.bagdatuly03@gmail.com>
2022-08-15 13:59:41 +0600

    vue

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,3 +1,13 @@
 <template>
 	<input class="border border-gray-300 w-full px-3 shadow-inner rounded-bl-sm rounded-tl-sm focus:border-blue-700 focus:outline-none" placeholder="Search" type="text">
 </template>
+
+<script>
+export default {
+	mounted () {
+		if (window.innerWidth < 640) {
+			this.$el.focus()
+		}
+	}
+}
+</script>
