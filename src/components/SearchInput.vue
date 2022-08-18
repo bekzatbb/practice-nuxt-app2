2093e68b63a515424ed692e6eb91b401d752ab67
Bekzat <bekzat.bagdatuly03@gmail.com>
2022-08-18 14:03:42 +0600

    vue

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,9 +1,25 @@
 <template>
-	<input class="border border-gray-300 w-full px-3 shadow-inner rounded-bl-sm rounded-tl-sm focus:border-blue-700 focus:outline-none" placeholder="Search" type="text">
+	<input :class="classes" placeholder="Search" type="text">
 </template>
 
 <script>
 export default {
+	data () {
+		return {
+			classes: [
+				'border', 
+				'border-gray-300', 
+				'w-full',
+				'px-3',
+				'shadow-inner',
+				'rounded-bl-sm',
+				'rounded-tl-sm',
+				'focus:border-blue-700',
+				'focus:outline-none'
+			]
+		}
+	},
+
 	mounted () {
 		if (window.innerWidth < 640) {
 			this.$el.focus()
